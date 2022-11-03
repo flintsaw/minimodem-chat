12518e8a4decfa401f507d97f653d1ff3bf0b364
flintsaw <76706713+flintsaw@users.noreply.github.com>
2022-11-03 15:07:40 -0500

    Update mmc-rx.sh
    Removed the unencrypted option.

--- mmc-rx.sh
+++ mmc-rx.sh
@@ -10,17 +10,8 @@ printf "*************************************"
 printf "\nlistening for data.\n"
 printf "*************************************\n\n"
 sleep 1
-
-    if [ $encrypt == "off" ]; then
-       minimodem -q --rx 30 -M $mark -S $space -c 2 --alsa=plughw:$card | tee -a tmp/rx-log
-       cat tmp/rx-log >> tmp/rx-main-log
-    fi
-
-    if [ $encrypt == "sym" ]; then
        minimodem -q --rx 30 -M $mark -S $space -c 2 --alsa=plughw:$card | tee -a tmp/rx-log.cpt
        ccrypt -d tmp/rx-log.cpt -K $pass
        cat tmp/rx-log >> tmp/rx-main-log
-    fi
-    
 done
 exit
