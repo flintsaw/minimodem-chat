26534fe4fb3a7a01bbbd190cc8eeea060c5c974b
flintsaw <76706713+flintsaw@users.noreply.github.com>
2022-11-03 14:30:05 -0500

    Update minimodem-chat.sh
    Replaced openssl with ccrypt.
    Capitalization and misc. polishing.

--- minimodem-chat.sh
+++ minimodem-chat.sh
@@ -19,24 +19,24 @@ chk_tubes() {
       if ! ping -c 1 eff.org > /dev/null 2>&1  ; then
          if ! ping -c 1 knowyourmeme.com > /dev/null 2>&1 ; then
              clear
-             printf "\n\ndo you have an internet connection???\n\n"
+             printf "\n\nDo you have an internet connection???\n\n"
              exit 1
          fi
       fi
   fi
-  printf "\n\ntubes working....\n\n"
+  printf "\n\nTubes working....\n\n"
 
 }
 
 get_aptpkg() {
-   printf "\nfetching $1 from apt\n"
+   printf "\nFetching $1 from APT\n"
    if ! apt-get -y install $1; then
        printf "\n\nAPT failed to install "$1", are your repos working?\nexiting...\n\n"
        exit 1
    fi
 }
 
-# func requires get_aptpkg func/ it test is bin exist if not tries to get it.
+# func requires get_aptpkg func/ checks if bin exists and if not tries to get it.
 test_bin() {
    if ! hash $1 >/dev/null 2>&1 ; then
       get_aptpkg $1
@@ -47,9 +47,9 @@ test_bin() {
 
 if [ ! -e /var/log/mmc-int ]; then
 
-  printf "\nIt seems that you have not run minimodem-chat before"
-  printf "\nwould you like to check if you missing any dependancies?"
-  printf "\nnote pulseaudio will not be installed by these checks"
+  printf "\nIt seems you have not run minimodem-chat before."
+  printf "\nWould you like to check for missing any dependancies?"
+  printf "\nNote: pulseaudio will not be installed by these checks"
   printf "\nas it is installed by default on most Ubuntu based distros"
   printf "\n(y/n)?\n"
   while true; do
@@ -64,11 +64,11 @@ if [ ! -e /var/log/mmc-int ]; then
                  test_bin tee
                  test_bin screen
                  test_bin minimodem
-                 test_bin openssl
-                 printf "\nit seems you are good to go.\n\n"
+                 test_bin ccrypt
+                 printf "\nIt seems you are good to go!\n\n"
                  date > /var/log/mmc-int
                  printf "minimodem-chat by xor-function\n\n" >> /var/log/mmc-int
-                 printf "\nchecks are done run again as a regular user.\n"
+                 printf "\nChecks are done. Run again as a regular user.\n"
                  exit
                  ;;
              * ) printf "\nNot a valid entry \nPlease answer y or n";;
@@ -82,4 +82,3 @@ if [ ! $? -eq 0 ]; then
 fi
 
 exit
-
